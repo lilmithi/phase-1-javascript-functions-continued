52053be30274ab5168cba2db03cdb95ce4245533
martin mithi <gathitumithi9000@gmail.com>
2022-08-16 15:01:22 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+mondayWork = function (activity = "go to the office") {
+  return `This Monday, I will ${activity}.`;
+};
+function wrapAdjective(symbol = "*") {
+  return function (emphatic) {
+    return `You are ${symbol}${emphatic}${symbol}!`;
+  };
+}
